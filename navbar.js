f973a4edc9b521f92bc0394cd11381bea6dda11b
elmohawk <mrhawk2007@gmail.com>
2026-07-12 20:46:11 +0200

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll",()=>{
+
+    const nav=document.querySelector(".navbar");
+
+    if(window.scrollY>50){
+
+        nav.classList.add("scrolled");
+
+    }else{
+
+        nav.classList.remove("scrolled");
+
+    }
+
+});
