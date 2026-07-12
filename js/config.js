4b240ae4e49d65e3169e09c610db6e845d72f798
elmohawk <mrhawk2007@gmail.com>
2026-07-12 19:29:54 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,17 @@
+/* ===========================
+   TMDB
+=========================== */
+
 const TMDB_API_KEY = "8b8937bf3e114fa3502358a4f090c0df";
 
-const TMDB_BASE =
-"https://api.themoviedb.org/3";
+const TMDB_BASE = "https://api.themoviedb.org/3";
+const IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
 
-const IMAGE_BASE =
-"https://image.tmdb.org/t/p/w500";
+/* ===========================
+   SUPABASE
+=========================== */
+
+const SUPABASE_URL = "https://exjgejujfxejjlbfizgz.supabase.co";
+
+const SUPABASE_ANON_KEY  =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
