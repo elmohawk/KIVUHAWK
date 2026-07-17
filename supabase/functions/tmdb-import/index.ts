d963ad2dc5e3a0eda9eadea7aed1024841e1e0e0
elmohawk <mrhawk2007@gmail.com>
2026-07-17 10:31:03 +0200

    Update index.ts

--- supabase/functions/tmdb-import/index.ts
+++ supabase/functions/tmdb-import/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "npm:@supabase/supabase-js@2";
 
-const TMDB_API_KEY = Deno.env.get("TMDB_API_KEY")!;
-const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
+const TMDB_API_KEY = Deno.env.get("8b8937bf3e114fa3502358a4f090c0df")!;
+const SUPABASE_URL = Deno.env.get("https://exjgejujfxejjlbfizgz.supabase.co")!;
 const SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 
 const supabase = createClient(
